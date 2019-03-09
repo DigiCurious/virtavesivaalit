1bc4b7bbbff880ec3917d705a9f512e0f54770b3
Oskari Polho <oskaripolho@Oskaris-MBP.lan>
2019-03-09 13:40:22 +0200

    viilauksia

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,33 @@ router.get('/privacy', function(req, res, next){
 });
 
 router.get('/', function(req, res) {
+  
+  var puolue;
+  var vaalipiiri;
+
+  puolue = JSON.stringify(req.cookies.puolue);
+  vaalipiiri = JSON.stringify(req.cookies.vaalipiiri);
+ 
+  console.log(puolue);
+
   Ehdokas.find({}, function(err, ehdokkaat){
   	if(err){
   		console.log(err)
   	}else{
-  		//console.log(ehdokkaat[3].kuva);
-  		res.render("index", { ehdokkaat: ehdokkaat})
+  		console.log(ehdokkaat);
+  		console.log(puolue);
+  		res.render("index", { ehdokkaat: ehdokkaat })
   	}
   });
-});
+  });
+
+router.post('/', function(req,res){
+	var puolue = req.body.puolue
+	var vaalipiiri = req.body.vaalipiiri
+
+	res.cookie('puolue', puolue);
+	res.cookie('vaalipiiri', vaalipiiri);
+	res.redirect("/");
+})
 
 module.exports = router;
